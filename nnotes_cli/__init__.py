6be284df7ca9719103f44708ad1846ddbc32b700
Daniel Moch <daniel@danielmoch.com>
2018-08-07 06:24:38 -0400

    Bump version to 0.1.1

--- nnotes_cli/__init__.py
+++ nnotes_cli/__init__.py
@@ -1,5 +1,5 @@
 __productname__ = 'nncli'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __copyright__ = "Copyright (c) 2018 Daniel Moch"
 __author__ = "Daniel Moch"
 __author_email__ = "daniel@danielmoch.com"
