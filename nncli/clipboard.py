e04cb3edce16f62781b0af30305c1a467fa0ea68
Daniel Moch <daniel@danielmoch.com>
2018-09-07 17:26:38 -0400

    Address pylint findings in clipboard.py
    Ref #10

--- nncli/clipboard.py
+++ nncli/clipboard.py
@@ -1,19 +1,23 @@
 # -*- coding: utf-8 -*-
-
+"""clipboard module"""
 import os
 from distutils import spawn
 
-class Clipboard(object):
+class Clipboard:
+    """Class implements copying note content to the clipboard"""
     def __init__(self):
         self.copy_command = self.get_copy_command()
 
-    def get_copy_command(self):
-        if (spawn.find_executable('xsel')):
+    @staticmethod
+    def get_copy_command():
+        """Defines the copy command based on the contents of $PATH"""
+        if spawn.find_executable('xsel'):
             return 'echo "%s" | xsel -ib'
-        if (spawn.find_executable('pbcopy')):
+        if spawn.find_executable('pbcopy'):
             return 'echo "%s" | pbcopy'
         return None
 
     def copy(self, text):
-        if (self.copy_command):
+        """Copies text to the system clipboard"""
+        if self.copy_command:
             os.system(self.copy_command % text)
